bda7b825cebb6e09997c0f1af5d91005218ed24f
angelo <angelochecked@gmail.com>
2020-02-02 15:22:34 +0100

    first implementation commands

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,6 @@ module Main where
 import Options.Applicative
 import Data.Semigroup ((<>))
 
-data Sample
-  = Hello [String]
-  | Goodbye
-  deriving (Eq, Show)
-
 main :: IO ()
 main = greet =<< execParser opts
   where
@@ -18,21 +13,47 @@ main = greet =<< execParser opts
      <> header "hello - a test for optparse-applicative" )
 
 greet :: Sample -> IO ()
-greet (Hello a) = print $ a
-greet Goodbye = print $ "Goodbye"
+greet (Sample (Hello a) False 1) = print $ a
+greet (Sample Init False 1) = print $ "Init"
+greet (Sample Template False 1) = print $ "Template"
+greet (Sample Push False 1) = print $ "Push"
+greet (Sample Pull False 1) = print $ "Pull"
 greet a = print $ a
 
-hello :: Parser Sample
-hello = Hello <$> many (argument str (metavar "TARGET..."))
+hello :: Parser Cmd
+hello = Hello <$> many (argument str (metavar "TARGET..." <> help "Target@"))
+
+data Cmd =  
+    Hello [String] 
+    | Init 
+    | Template 
+    | Push 
+    | Pull 
+    deriving (Eq, Show)
+
+data Sample = Sample 
+      { cmd     :: Cmd
+      , quiet      :: Bool
+      , enthusiasm :: Int } 
+    deriving (Eq, Show)
 
 sample :: Parser Sample
-sample = subparser
-       ( command "hello" (info hello (progDesc "Print greeting"))
-      <> command "goodbye" (info (pure Goodbye) (progDesc "Say goodbye"))
+sample = Sample
+     <$> subparser
+       ( command "hello" (info (hello) (progDesc "Print greeting"))
+      <> command "init" (info (pure Init) (progDesc "Say Init"))
+      <> command "template" (info (pure Template) (progDesc "Say Template"))
+      <> command "push" (info (pure Push) (progDesc "Say Push"))
+      <> command "pull" (info (pure Pull) (progDesc "Say Pull"))
        )
-      <|> subparser
-       ( command "bonjour" (info hello (progDesc "Print greeting"))
-      <> command "au-revoir" (info (pure Goodbye) (progDesc "Say goodbye"))
-      <> commandGroup "French commands:"
-      <> hidden
-       )
+     <*> switch
+          ( long "quiet"
+         <> short 'q'
+         <> help "Whether to be quiet" )
+      <*> option auto
+          ( long "enthusiasm"
+         <> help "How enthusiastically to greet"
+         <> showDefault
+         <> value 1
+         <> metavar "INT" )
+
